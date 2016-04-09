e8e64836c3dceb84a6cf56c0ad4d27df2fed8c35
demiurgosoft <demiurgosoft@hotmail.com>
2016-04-10 00:59:35 +0200

    token expire date changed to 1 month, v 0.1.8

--- config/server.js
+++ config/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 module.exports = {
 	port: process.env.PORT || 8081, //server port
 	secret: process.env.SECRET || "dontpanic42", //secret
-	tokenExpire: 3600, //token expiration time (in seconds)
+	tokenExpire: "30 days", //token expiration time (in seconds)
 	//setups basic server (sets bodyparser)
 	setup: function(app) {
 		app.use(bodyParser.json()); // get information from body
